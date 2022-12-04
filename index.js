80d9d4452cffd87b18512ca7a4652acec4de9e76
Luke <laj.baxter@gmail.com>
2022-12-04 13:58:01 +1100

    jkh

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
+let randomMeal = ["https://madhumanbeing.github.io/veggieBowl.html",
+"https://madhumanbeing.github.io/dhal.html",
+"https://madhumanbeing.github.io/morroccanChickpea.html",
+"https://madhumanbeing.github.io/zucchiniFritters.html",
+"https://madhumanbeing.github.io/summerChickpea.html",
+"https://madhumanbeing.github.io/vietnameseNoodles.html",
+"https://madhumanbeing.github.io/waldorfSalad.html",
+"https://madhumanbeing.github.io/peanutSoba.html",
+"https://madhumanbeing.github.io/whiteBeanSalad.html",
+"https://madhumanbeing.github.io/Eggplant%20dumplings.html",
+"https://madhumanbeing.github.io/frittata.html",
+"https://madhumanbeing.github.io/freekeh.html",
+"https://madhumanbeing.github.io/crispyRoastPotatoes.html",
+"https://madhumanbeing.github.io/khaoSoi.html",
+"https://madhumanbeing.github.io/beefBrisket.html",
+"https://madhumanbeing.github.io/ramen.html",
+"https://madhumanbeing.github.io/thaiFishcakes.html"];
 
-let menuToggle = document.querySelector('.menuToggle');
-let navigation = document.querySelector('.navigation');
-menuToggle.onclick = function(){
-navigation.classList.toggle('active');
-};
+function shuffle(randomMeal) {
+    let currentIndex = randomMeal.length,  randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [randomMeal[currentIndex], randomMeal[randomIndex]] = [
+        randomMeal[randomIndex], randomMeal[currentIndex]];
+    }
+  
+    return randomMeal;
+  }
+
+  function randomize(){
+    var myarray = new Array("item1", "item2", "item3");
+    var randomId =  Math.floor((Math.random()*myarray.length));
+    var randomItem = myarray[randomid];
+  
+    alert(randomItem);
+  }
