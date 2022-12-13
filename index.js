94241dcfd63d781e59ee60af4ecb9281c587397c
Luke <laj.baxter@gmail.com>
2022-12-13 16:30:22 +1100

    fingers crossed

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 let linksArray = [
-"https://madhumanbeing.github.io/veggieBowl.html",
-"https://madhumanbeing.github.io/dhal.html",
-"https://madhumanbeing.github.io/morroccanChickpea.html",
-"https://madhumanbeing.github.io/zucchiniFritters.html",
-"https://madhumanbeing.github.io/summerChickpea.html",
-"https://madhumanbeing.github.io/vietnameseNoodles.html",
-"https://madhumanbeing.github.io/waldorfSalad.html",
-"https://madhumanbeing.github.io/peanutSoba.html",
-"https://madhumanbeing.github.io/whiteBeanSalad.html",
-"https://madhumanbeing.github.io/Eggplant%20dumplings.html",
-"https://madhumanbeing.github.io/frittata.html",
-"https://madhumanbeing.github.io/freekeh.html",
-"https://madhumanbeing.github.io/crispyRoastPotatoes.html",
-"https://madhumanbeing.github.io/khaoSoi.html",
-"https://madhumanbeing.github.io/beefBrisket.html",
-"https://madhumanbeing.github.io/ramen.html",
-"https://madhumanbeing.github.io/thaiFishcakes.html"];
+"veggieBowl.html",
+"dhal.html",
+"morroccanChickpea.html",
+"zucchiniFritters.html",
+"summerChickpea.html",
+"vietnameseNoodles.html",
+"waldorfSalad.html",
+"peanutSoba.html",
+"whiteBeanSalad.html",
+"Eggplant%20dumplings.html",
+"frittata.html",
+"freekeh.html",
+"crispyRoastPotatoes.html",
+"khaoSoi.html",
+"beefBrisket.html",
+"ramen.html",
+"thaiFishcakes.html"];
 
 function randomMeal() {
   var i = parseInt(Math.random() * linksArray.length);
   location.href = linksArray[i];
-  document.getElementById("link").innerHTML = randomMeal();
 }
+
 
 
